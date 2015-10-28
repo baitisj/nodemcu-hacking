526b47e5aaa0cca3c83567ed64849ef401db100d
Jeff 'Raid' Baitis <jeff@baitis.net>
2015-10-28 01:50:56 -0700

    Main now functional

--- main.lua
+++ main.lua
@@ -3,21 +3,18 @@ do
   local m = require("morse")
   m.setPort(0)
   local pin = 4
-  local port = 80
+  local port = 60124
   local t = {}
   t[dht.OK] = "OK"
   t[dht.ERROR_CHECKSUM] = "ERROR_CHECKSUM"
   t[dht.ERROR_TIMEOUT] = "TIMEOUT"
 
   function postTemp() 
-    sk=net.createConnection(net.TCP,0)
-    sk:on("receive", function(sck,c) print(c) end)
-    sk:connect(port,ip)
 
     status,temp,humi,temp_decimial,humi_decimial = dht.read(pin)
-    m.queue("T"..math.floor(temp))
+    --m.queue("T"..math.floor(temp))
     local ip="192.168.8.2"
-    header = ("node,","status,","uptime,","temp,","humidity")
+    header = "node,status,uptime,temp,humidity"
     dat = string.format(
       "%d,%s,%d,%d.%03d,%d.%03d\n",
       node.chipid(),
@@ -29,11 +26,29 @@ do
       humi_decimial
     )
 
+    if temp < 0 then
+      m.send("N"..temp)
+    else
+      m.send(""..temp)
+    end
+
+    sk=net.createConnection(net.TCP,0)
+    sk:on("receive", function(sck,c) print(c) end)
     sk:on("connection", function(sck,c)
-      sk:send("GET /iot/update?data="..dat.."HTTP/1.1\nHost: "..ip.."\nConnection: keep-alive\nAccept: */*\n\n")
-      sk:close()
-      m.queue("SK")
+      sck:send("GET /iot/update?data="..dat.." HTTP/1.1\r\nHost: "..ip.."\r\nConnection: keep-alive\r\nAccept: */*\r\n\r\n")
     end)
+    sk:on("sent", function(sck,c)
+      sck:close()
+    end)
+    sk:connect(port,ip)
 
     print (header)
     print (dat)
+    m.setNotify(function() tmr.alarm(1,10000,0,postTemp) end)
+  end
+  -- expose
+  M = {
+    postTemp = postTemp
+  }
+end
+M.postTemp()
