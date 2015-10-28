360611c3bf337edf7d4ae0265e7a3a61e85546bb
Jeff 'Raid' Baitis <jeff@baitis.net>
2015-10-27 22:06:34 -0700

    Removed requirement for ring buffer Broke out table into its own file

--- morse.lua
+++ morse.lua
@@ -1,54 +1,15 @@
 local M
 do
-  local wb=require ("ringbuffer")
-  local t = {}
-	t[48]='-----' --0
-	t[49]='.----'
-	t[50]='..---'
-	t[51]='...--'
-	t[52]='....-'
-	t[53]='.....'
-	t[54]='-....'
-	t[55]='--...'
-	t[56]='---..'
-	t[57]='----.' --9
-	t[65]='.-' -- A
-	t[66]='-...'
-	t[67]='-.-.'
-	t[68]='-..'
-	t[69]='.'
-	t[70]='..-.'
-	t[71]='--.'
-	t[72]='....'
-	t[73]='..'
-	t[74]='.---'
-	t[75]='-.-'
-	t[76]='.-..'
-	t[77]='--'
-	t[78]='-.'
-	t[79]='---'
-	t[80]='.--.'
-	t[81]='--.-'
-	t[82]='.-.'
-	t[83]='...'
-	t[84]='-'
-	t[85]='..-'
-	t[86]='...-'
-	t[87]='.--'
-	t[88]='-..-'
-	t[89]='-.--'
-	t[90]='--..' -- Z
-
-  local p=0
-  local active=0
-  local br=600
-
-  local curWord=nil
-
-  local g = require("gpio")
-  local alrm = function(t,nxt) tmr.alarm(1,t,0,nxt) end
+  local tbl = require ("morse_table")
+  local active=false
   local notify=nil
   local debug=nil
+
+  local br=600
+
+  local p=0
+  local g = require("gpio")
+  local alrm = function(t,nxt) tmr.alarm(1,t,0,nxt) end
 
   function cancel()
     tmr.cancel(1) 
@@ -70,24 +31,20 @@ do
     notify=cb
   end
 
-  function queue(str)
-    r = wb.push(str:upper())
-    if r ~= nil then
-      if active == 0 then
-        active = 1
-        toot(wb.pop(), 1, wordDone)
-      end
+  function send(str)
+    if active == true then
+      return nil
     end
-    return r
+    active = true
+    toot(str:upper(), 1, wordDone)
   end
 
   function toot(w,idx,cb)
     if idx > w:len() then
       return wpause(cb)
     end
-    bval=w:byte(idx)
-    ms=t[bval]
-    if debug ~= nil then print(bval,ms) end
+    ms=tbl.getMCode(w,idx)
+    if debug ~= nil then print(ms) end
     doBlinks(ms,1,function() toot(w,idx+1,cb) end)
   end
 
@@ -109,12 +66,8 @@ do
   
   function wordDone()
     wpause(function()
-      nxt = wb.pop()
-      if nxt ~= nil then wpause(function() toot(nxt,1,wordDone) end) 
-      else
-        active = 0
-        if notify ~= nil then notify() end
-      end
+      active = false
+      if notify ~= nil then notify() end
     end)
   end
 
@@ -143,7 +96,7 @@ do
   -- expose
   M = {
     setPort = setPort,
-    queue = queue,
+    send = send,
     cancel = cancel,
     setDebug = setDebug,
     setNotify = setNotify
