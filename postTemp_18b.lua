022136c5f09b0758b315454b1875345b6cfb6bb4
Jeff 'Raid' Baitis <jeff@baitis.net>
2015-11-11 12:15:25 -0800

    Sends data to the website fine the first time. Subsequent attemps result in crash

--- postTemp_18b.lua
+++ postTemp_18b.lua
@@ -4,7 +4,6 @@ local host = "192.168.8.2"
 local port = 60124
 local net = net
 local ds=require("ds18b20")
-local nextCB = nil
 local sk = nil
 local r = nil
 local alrm = function(t,nxt) tmr.alarm(5,t,0,nxt) end
@@ -21,7 +20,7 @@ function postTemp()
 end
 
 function itr(idx,sck,cb)
-  print ("->itr")
+  print ("->itr "..idx)
   if idx > #r then
     if sck ~= nil then sck:close() end
     if cb ~= nil then cb() end
@@ -37,7 +36,7 @@ function itr(idx,sck,cb)
     t = r[idx][2].."."..r[idx][3]
   end
   if id and t then
-    send(itr,sck,id,t)
+    send(idx,sck,id,t)
     return
   end
   alrm(1,function() itr(idx+1,sck,cb) end)
@@ -48,14 +47,12 @@ function start(cb)
   sk = sk or net.createConnection(net.TCP,0)
   sk:on("receive", function(sck,c) print(c) end)
   sk:on("connection", function(sck,c) itr(1,sck,cb) end)
-  sk:on("sent", function(sck,c) nextCB(sck) end)
   sk:connect(port,host)
 end
 
-function send(itr,sck,id,t)
+function send(i,sck,id,t)
   print("->send")
-  nextCB = function(sck) alrm(1,itr(i+1,sck,cb)) end
-  sk:on("sent", function(sck,c) nextCB(sck) end)
+  sk:on("sent", function(sck,c) itr(i+1,sck,cb) end)
   sck:send("GET /iot/update?data="..id..","..t.." HTTP/1.1\r\nHost: "..host.."\r\nConnection: keep-alive\r\nAccept: */*\r\n\r\n")
 end
 -- expose
